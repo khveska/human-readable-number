7192f51f2dedd408717214973a7685be9eb4b75f
Alex Vol <yazukata.khveska@gmail.com>
2022-07-05 15:34:40 +0300

    fix: replace switch statements with if conditions

--- src/index.js
+++ src/index.js
@@ -13,22 +13,16 @@ module.exports = function toReadable(number) {
 function numberToReadable(numberStr) {
     const number = parseInt(numberStr);
 
-    switch (number) {
-        case number <= 12:
-            return digits[number];
-        case number < 20:
-            return (teens[numberStr.slice(-1)] || digits[numberStr.slice(-1)]) + TEEN_POSTFIX;
-        case number < 100:
-            return (
-                (tens[numberStr[0]] || teens[numberStr[0]] || digits[numberStr[0]]) +
-                TEN_POSTFIX +
-                numberToReadable(numberStr.slice(1))
-            );
-        case number < 1000:
-            return digits[numberStr[0]] + HUNDRED + numberToReadable(numberStr.slice(1));
-        case number < 10000:
-            return digits[numberStr[0]] + THOUSAND + numberToReadable(numberStr.slice(1));
-        default:
-            throw new Error("Current implementation supports only numbers in the range [0 - 9999]");
-    }
+    if (number <= 12) return digits[number];
+    if (number < 20) return (teens[numberStr.slice(-1)] || digits[numberStr.slice(-1)]) + TEEN_POSTFIX;
+    if (number < 100)
+        return (
+            (tens[numberStr[0]] || teens[numberStr[0]] || digits[numberStr[0]]) +
+            TEN_POSTFIX +
+            numberToReadable(numberStr.slice(1))
+        );
+    if (number < 1000) return digits[numberStr[0]] + HUNDRED + numberToReadable(numberStr.slice(1));
+    if (number < 10000) return digits[numberStr[0]] + THOUSAND + numberToReadable(numberStr.slice(1));
+
+    throw new Error("Current implementation supports only numbers in the range [0 - 9999]");
 }
