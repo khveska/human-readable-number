58390ad9873d417c19f8d767840a90a92d2b901c
Alex Vol <yazukata.khveska@gmail.com>
2022-07-05 15:25:17 +0300

    init: implement human readable number task

---
+++ src/constants.js
@@ -0,0 +1,34 @@
+const digits = {
+  0: "",
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+};
+
+const teens = {
+  2: "twen",
+  3: "thir",
+  4: "four",
+  5: "fif",
+  8: "eigh",
+};
+
+const tens = { 4: "for" };
+
+
+const ZERO = "zero";
+const TEEN_POSTFIX = "teen";
+const TEN_POSTFIX = "ty ";
+const HUNDRED = " hundred ";
+const THOUSAND = " thousand ";
+
+module.exports = {ZERO, digits, teens, tens, TEEN_POSTFIX, TEN_POSTFIX, HUNDRED, THOUSAND};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function toReadable (number) {
-  
+const { ZERO, digits, teens, tens, TEEN_POSTFIX, TEN_POSTFIX, HUNDRED, THOUSAND } = require("./constants");
+
+module.exports = function toReadable(number) {
+    if (!Number.isFinite(number)) {
+        throw new Error("Number is not finite and can't be converted to human readable form");
+    }
+
+    if (number === 0) return ZERO;
+
+    return numberToReadable(number.toString()).trim();
+};
+
+function numberToReadable(numberStr) {
+    const number = parseInt(numberStr);
+
+    switch (number) {
+        case number <= 12:
+            return digits[number];
+        case number < 20:
+            return (teens[numberStr.slice(-1)] || digits[numberStr.slice(-1)]) + TEEN_POSTFIX;
+        case number < 100:
+            return (
+                (tens[numberStr[0]] || teens[numberStr[0]] || digits[numberStr[0]]) +
+                TEN_POSTFIX +
+                numberToReadable(numberStr.slice(1))
+            );
+        case number < 1000:
+            return digits[numberStr[0]] + HUNDRED + numberToReadable(numberStr.slice(1));
+        case number < 10000:
+            return digits[numberStr[0]] + THOUSAND + numberToReadable(numberStr.slice(1));
+        default:
+            throw new Error("Current implementation supports only numbers in the range [0 - 9999]");
+    }
 }
